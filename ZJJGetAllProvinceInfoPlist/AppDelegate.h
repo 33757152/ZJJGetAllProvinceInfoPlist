483ded5bbd002fb63f63af316f1e101e4ce9c50f
张锦江 <33757152@qq.com>
2018-04-26 14:33:18 +0800

    Initial Commit

---
+++ ZJJGetAllProvinceInfoPlist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJGetAllProvinceInfoPlist
+//
+//  Created by 张锦江 on 2018/4/26.
+//  Copyright © 2018年 xtayqria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJJGetAllProvinceInfoPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJJGetAllProvinceInfoPlist
+//
+//  Created by 张锦江 on 2018/4/26.
+//  Copyright © 2018年 xtayqria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
